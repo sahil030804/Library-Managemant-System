0e362bb1d21e8934a86f7780790ed033bd1f7178
Sahil Ranpariya <sahilranpariya60@gmail.com>
2024-12-13 11:57:13 +0530

    feat/add book feature by only admin
    added book adding feature by only admin access this route otherwise any member cantry to access this then give error that you are not authorized also add JOI validation for user input and also check sibn first in DB for prevent dupilication of book

---
+++ src/components/book/book.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bookController from "./book.controller.js";
+import bookValidation from "./book.validation.js";
+import validation from "../../middleware/validation.js";
+import auth from "../../middleware/auth.js";
+
+const router = express.Router();
+
+router.post(
+  "/books",
+  auth.userAuthenticate,
+  auth.accessRole(["admin"]),
+  validation.validate(bookValidation.addBookValidate),
+  bookController.addBook
+);
+
+export default router;
